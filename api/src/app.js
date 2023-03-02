dc6880c5e66323e225da57e96bd88732f3cf096e
sbaccon <sbaccon.absti@gmail.com>
2023-03-02 16:24:49 -0300

    cambios en config 2

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const cors = require('cors');
 
 require("./db.js");
 
@@ -14,24 +10,12 @@ const server = express();
 
 server.name = "API";
 
-http.createServer((req, res) => {
-  res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url });
-  res.end();
-}).listen(80);
-
-const corsOptions = {
-  origin: "https://13.42.129.29",
-  credentials: true,
-};
-
-server.set('trust proxy', 1);
-server.use(cors(corsOptions));
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.get("origin"));
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -50,9 +34,4 @@ server.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 
-const options = {
-  key: fs.readFileSync('/path/to/private/key.pem'),
-  cert: fs.readFileSync('/path/to/certificate.pem')
-};
-
-https.createServer(options, server).listen(443);
+module.exports = server;
